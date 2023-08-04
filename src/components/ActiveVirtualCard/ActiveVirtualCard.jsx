95f77dd9e2543cfbcb3bd9417d3d41c8ac02f999
Dima <dmitriy.movchan.02@gmail.com>
2023-08-04 16:44:03 +0300

    change pin api

--- src/components/ActiveVirtualCard/ActiveVirtualCard.jsx
+++ src/components/ActiveVirtualCard/ActiveVirtualCard.jsx
@@ -27,7 +27,7 @@ export const ActiveVirtualCard = () => {
       await axios.post('https://dev2.fin.forkflow.com/fe/physical-card/activate', request);
       console.log(123)
 
-      // updateData(...request)
+      updateData(request)
       setSuccess(true);
     } catch (error) {
       if (error.response?.status === 401) {
