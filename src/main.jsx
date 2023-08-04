166d02e71350fec76d76f285b57565fa95b4f333
Dima <dmitriy.movchan.02@gmail.com>
2023-08-04 18:27:54 +0300

    fix focus and button type

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 
+const metaTag = document.createElement('meta');
+metaTag.httpEquiv = 'Permissions-Policy';
+metaTag.content = 'interest-cohort=()';
+document.head.appendChild(metaTag);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
